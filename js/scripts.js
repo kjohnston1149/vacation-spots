6fc802214a09b575df8b097b634b7b29c3187d21
Kevin Johnston and Mark Lawson <kevin@>
2016-08-22 09:12:00 -0700

    create the Destination object constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,13 @@
+  // bussiness end
+function Destination(place, country, date, info){
+  this.placeName = place;
+  this.country = country;
+  this.date = date;
+  this.info = info;
+}
+
+  // front end
 $(function() {
-  
+
+
 });
