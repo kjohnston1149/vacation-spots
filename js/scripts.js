8f34c0226a9045983485e5a900b3ae292d50c2a1
Kevin Johnston and Mark Lawson <kevin@>
2016-08-22 10:27:00 -0700

    add an option to link and display an image

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
   // bussiness end
-function Destination(place, country, date, info){
+function Destination(place, country, date, info, image){
   this.placeName = place;
   this.country = country;
   this.date = date;
   this.info = info;
+  this.image = image;
 }
 
   // front end
@@ -14,20 +15,22 @@ $(function() {
     var country = $("#country").val();
     var date = $("#date").val();
     var info = $("#info").val();
-    var newDestination = new Destination(placeName, country, date, info);
-    console.log(newDestination);
+    var image = $("#image").val();
+    var newDestination = new Destination(placeName, country, date, info, image);
 
     $("ul#placeList").append("<li><span class='listItem'>" + newDestination.placeName + "</span></li>");
 
     $(".listItem").last().click(function() {
       $("#showTravelInfo").show();
       $("#showTravelInfo h2").text(newDestination.placeName);
+      $(".image").html("<img src='" + image + "'>");
       $(".country").text(newDestination.country);
       $(".date").text(newDestination.date);
       $(".info").text(newDestination.info);
     });
 
     $("#placeName").val("");
+    $("#image").val("");
     $("#country").val("");
     $("#date").val("");
     $("#info").val("");
